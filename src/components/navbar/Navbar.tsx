c7804524b7f0c10b57ab14d73d693060ddb99724
wagh <wagh1.2.3.002@gmail.com>
2025-01-22 10:22:33 +0530

    update font family to MonumentExtended, add react-lottie for animations, and refine navbar layout

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,46 @@
-'use client'
-import Image from 'next/image';
-
+"use client";
+import Image from "next/image";
+import Lottie from "react-lottie";
+import location from "../../app/location.json";
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center px-16 h-[117px] bg-[#0C041F] backdrop-blur-sm">
-      <div className="flex items-center gap-8">
-        <Image 
-          src="/AutoExplorer.svg" 
-          alt="Auto Explorer" 
-          width={77} 
-          height={80} 
+      <div className="flex items-center">
+        <Image
+          src="/AutoExplorer.svg"
+          alt="Auto Explorer"
+          width={77}
+          height={80}
           className="object-contain"
         />
-        <div className="flex gap-6 text-white">
-          <a href="#home" className="text-xl font-monument font-[Monument_Extended]">Auto Explorer</a>
+        <div className="flex text-white">
+          <a href="#home" className="text-2xl">
+            Auto Explorer
+          </a>
         </div>
       </div>
-      
+
       <div className="flex items-center gap-6">
         <div className="relative">
-          <input type="search" className="w-[360px] h-14 bg-[#ECE6F0] rounded-full px-12" placeholder="Search" />
+          <input
+            type="search"
+            className="w-[360px] h-14 bg-[#ECE6F0] rounded-full px-12"
+            placeholder="Search"
+          />
+        </div>
+        <div className="cursor-pointer [&>div]:invert">
+          <Lottie
+            options={{
+              animationData: location,
+              rendererSettings: {
+                preserveAspectRatio: "xMidYMid slice",
+              },
+              autoplay: true,
+              loop: true,
+            }}
+            height={50}
+            width={50}
+          />
         </div>
         <div className="flex items-center gap-4">
           <button className="px-5 py-2 border border-[#AF52DE] text-white rounded-lg font-abril">
