13f9a4624551b44259b437b2ccf1ef59b6ef8c22
DEVEN WAGH <wagh1.2.3.002@gmail.com>
2025-02-07 00:49:57 +0530

    refactor: simplify divider layout in registration page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -155,12 +155,8 @@ const Register = () => {
             </button>
 
             {/* Divider */}
-            <div className="relative my-4">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-gray-700"></div>
-              </div>
-              <div className="relative flex justify-center text-sm">
-                <span className="px-2 text-gray-400 bg-gray-950">Or continue with</span>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center w-full border-t border-gray-700">
               </div>
             </div>
 
